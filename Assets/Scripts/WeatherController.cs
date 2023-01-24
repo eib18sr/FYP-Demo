5d0dcb557f0858481df965779c2fb91993b427bb
eib18sr <sroberts6@sheffield.ac.uk>
2023-01-24 12:56:01 +0000

    Initial XR Interaction Toolkit work for Android

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -10,10 +10,17 @@ public class WeatherController : MonoBehaviour
     private OpenWeatherApiClient weatherAPI;
     [SerializeField] private TextMeshProUGUI weatherText;
     [SerializeField] private TextAsset keyFile;
+    [SerializeField] private string defaultPlace;
 
-    private void Start()
+    private void Awake()
     {
         weatherAPI = new OpenWeatherApiClient(keyFile.text);
+    }
+
+    private void OnEnable()
+    {
+        if (string.IsNullOrEmpty(defaultPlace)) return;
+        SetWeatherText(defaultPlace);
     }
 
     private async Task<QueryResponse> ReportCurrentWeather(string place)
@@ -22,22 +29,24 @@ public class WeatherController : MonoBehaviour
         return query;
     }
 
+    private string WeatherString(QueryResponse queryResponse)
+    {
+        return string.Format("The weather in {0}, {1} is currently {2} °C and {3}", queryResponse.Name, queryResponse.Sys.Country, queryResponse.Main.Temperature.CelsiusCurrent, GetFirstWord(queryResponse.WeatherList[0].Description));
+    }
+
+    private string GetFirstWord(string description)
+    {
+        return description.Split(' ')[0];
+    }
+
+    /// <summary>
+    /// Sets the TMPro element to a weather report
+    /// </summary>
+    /// <param name="place">The location which to report</param>
     public async void SetWeatherText(string place)
     {
         var weather = await ReportCurrentWeather(place);
 
         weatherText.text = WeatherString(weather);
     }
-
-    public string WeatherString(QueryResponse queryResponse)
-    {
-        return string.Format("The weather in {0}, {1} is currently {2} °C and {3}", queryResponse.Name, queryResponse.Sys.Country, queryResponse.Main.Temperature.CelsiusCurrent, GetFirstWord(queryResponse.WeatherList[0].Description));
-    }
-
-    public string GetFirstWord(string description)
-    {
-        return description.Split(' ')[0];
-    }
-
-
 }
